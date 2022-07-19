3198edbb8c635be28a3284dab3b5c053027cac37
YUN JIN LEE <96698362+yunjin-21@users.noreply.github.com>
2022-07-19 23:26:07 +0900

    Update WelcomeFace.js

--- WelcomeFace.js
+++ WelcomeFace.js
@@ -1,82 +1,82 @@
-import React from "react";
-import {StyleSheet, Text, View,Image,SafeAreaView,Alert, Button } from "react-native";
+import React, { useState, useEffect } from 'react';
+import * as ImagePicker from 'expo-image-picker';
+import {StyleSheet, Text, View,Image,SafeAreaView,Alert, Button, Platform } from "react-native";
 import { NavigationContainer } from '@react-navigation/native';
 
-const WelcomeFace = ({navigation}) => {
-    return (
-        <SafeAreaView style ={styles.container}>
-           <Text style={styles.title}>CHANGE</Text>
-            <Image  source={{uri: 'https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2FMjAxOTAxMDZfMTM5%2FMDAxNTQ2NzU1MzY1NjU3.Zfww7opoXiVhBi3KUhMBzkomXBYLR6N_U0lySDbsejQg.VBWtfCc1hNQfJ6j6lND_tKyLnSLHj4em5HfH0d3HEFYg.JPEG.oow98%2FEmma_Stone_-_19th_Annual_AFI_Awards_in_Beverly_Hills2C_California_on_January.jpg&type=a340'}}
-                style={{
-                resizeMode: "contain",
-                height: 450,
-                width: 350}}/>
 
-            <View style={[{ width: "90%", height: "20%",}]}>
+export default function WelcomeFace({navigation}) {
+  const [image, setImage] = useState(null);
+
+  const pickImage = async () => {
+    // No permissions request is necessary for launching the image library
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    console.log(result);
+
+    if (!result.cancelled) {
+      setImage(result.uri);
+    }
+  };
+
+  return (
+    <SafeAreaView style ={styles.container}>
+    
+       
+     
+        <Text style={styles.title}>CHANGE</Text>
               <Button
-                title="Characterize!"
+                title="Upload Your Image"
+                color="rgb(250, 209, 169)"
+                onPress={pickImage}
+              />
+                 <Button
+                 title="Let's see your Disney Face"
                 color="rgb(250, 209, 169)"
                 onPress={() => navigation.navigate('WelcomeDisney')} 
               />
-            </View>
-        </SafeAreaView>
-      );
+            
+      {image && <Image source={{ uri: image }} style={{ width: 350, height: 350 }} />}
+    
+    </SafeAreaView>
+  );
 }
 
+
+
 const styles = StyleSheet.create({
-    container:{
-        flex: 1,
-        backgroundColor: 'rgb(252,233,212) ',
-        alignItems:"center",
-        justifyContent: "space-between",
+  container:{
+      flex: 1,
+      backgroundColor: 'rgb(252,233,212) ',
+      alignItems:"center",
+      justifyContent: "space-between",
 
-      },
+    },
 
-      title: {
-        marginTop: 0.001,
-        width: '100%',
-        height: '10%',
-        backgroundColor: 'rgb(240, 217, 215)',
+    title: {
+      marginTop: 0.001,
+      width: '100%',
+      height: '10%',
+      backgroundColor: 'rgb(240, 217, 215)',
 
-        top: '0.00000000002%',
-        color: "black",
-        fontSize: 70,
-        fontWeight: "bold",
-        fontStyle: 'italic',
-        textAlign: "center",
-      },
-      image: {
-        /*resizeMode: "cover",*/
-        /*width: '0.5%',
-        height: '0.5%',*/
-        resizeMode: 'contain',
+      top: '0.00000000002%',
+      color: "black",
+      fontSize: 70,
+      fontWeight: "bold",
+      fontStyle: 'italic',
+      textAlign: "center",
+    },
+    image: {
+      /*resizeMode: "cover",*/
+      /*width: '0.5%',
+      height: '0.5%',*/
+      resizeMode: 'contain',
 
-        justifyContent: "center",
+      justifyContent: "center",
 
-      }
+    }
 });
-
-
-export default WelcomeFace;
-
-/*,
-      image: {
-        /*resizeMode: "cover",*/
-        /*width: '0.5%',
-        height: '0.5%',*/
-      /*  resizeMode: 'contain',
-      
-        justifyContent: "center",
-    
-      }*/
-
-    /*  <Image 
-      /*style={styles.cover}*/
-      /*source={require("./assets/emma.jpg")}
-      style={{
-        resizeMode: "contain",
-        height: 200,
-        width: 300
-      }}
-      
-  />*/
