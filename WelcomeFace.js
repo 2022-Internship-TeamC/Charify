d38e4856c298d7fad77670c6fad9c03a6fc4dc8b
YUN JIN LEE <96698362+yunjin-21@users.noreply.github.com>
2022-07-24 23:59:30 +0900

    Update WelcomeFace.js

--- WelcomeFace.js
+++ WelcomeFace.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from 'react';
+import {StyleSheet, Text, View,Image,SafeAreaView,Alert, Button, Platform, TouchableOpacity} from "react-native";
 import * as ImagePicker from 'expo-image-picker';
-import {StyleSheet, Text, View,Image,SafeAreaView,Alert, Button, Platform } from "react-native";
 import { NavigationContainer } from '@react-navigation/native';
+
 
 
 export default function WelcomeFace({navigation}) {
@@ -21,62 +22,70 @@ export default function WelcomeFace({navigation}) {
     if (!result.cancelled) {
       setImage(result.uri);
     }
-  };
+};
+
 
   return (
     <SafeAreaView style ={styles.container}>
-    
-       
-     
         <Text style={styles.title}>CHANGE</Text>
-              <Button
-                title="Upload Your Image"
-                color="rgb(250, 209, 169)"
-                onPress={pickImage}
-              />
-                 <Button
+    <View style={styles.container}>
+   
+      
+
+      <TouchableOpacity onPress={pickImage} style={styles.button}>
+        <Text style={styles.buttonText}>Pick a photo</Text>
+      </TouchableOpacity>
+
+       <Button
                  title="Let's see your Disney Face"
-                color="rgb(250, 209, 169)"
+                color="black"
                 onPress={() => navigation.navigate('WelcomeDisney')} 
               />
-            
-      {image && <Image source={{ uri: image }} style={{ width: 350, height: 350 }} />}
-    
+              
+    </View>
+
+    {image && <Image source={{ uri: image }} style={{ width: 300, height: 300 , alignItems: 'center',
+    justifyContent: 'center',}} />}
     </SafeAreaView>
   );
-}
-
+  }
 
 
 const styles = StyleSheet.create({
-  container:{
-      flex: 1,
-      backgroundColor: 'rgb(252,233,212) ',
-      alignItems:"center",
-      justifyContent: "space-between",
+  container: {
+    flex: 1,
+    backgroundColor: 'rgb(240, 217, 215)',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    width: '100%',
+    height: '10%',
+    backgroundColor: 'rgb(240, 217, 215)',
 
-    },
-
-    title: {
-      marginTop: 0.001,
-      width: '100%',
-      height: '10%',
-      backgroundColor: 'rgb(240, 217, 215)',
-
-      top: '0.00000000002%',
-      color: "black",
-      fontSize: 70,
-      fontWeight: "bold",
-      fontStyle: 'italic',
-      textAlign: "center",
-    },
-    image: {
-      /*resizeMode: "cover",*/
-      /*width: '0.5%',
-      height: '0.5%',*/
-      resizeMode: 'contain',
-
-      justifyContent: "center",
-
-    }
+    color: "black",
+    fontSize: 70,
+    fontWeight: "bold",
+    fontStyle: 'italic',
+    textAlign: "center",
+        },
+  button: {
+    backgroundColor: 'pink',
+    padding: 20,
+    borderRadius: 5,
+  },
+  buttonText: {
+    fontSize: 20,
+    color: '#fff',
+  },
+  thumbnail: {
+    width: 400,
+    height: 400,
+    resizeMode: 'contain',
+  },
+  logo: {
+    width: 350,
+    height: 350,
+    marginBottom: 20,
+  },
 });
