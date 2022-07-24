a3c38c47f4ca9b5410ad37e316dd2ac17aee0c4a
YUN JIN LEE <96698362+yunjin-21@users.noreply.github.com>
2022-07-24 23:53:52 +0900

    Update AboutUs.js

--- AboutUs.js
+++ AboutUs.js
@@ -1,20 +1,38 @@
 import React from 'react';
-import { View, StyleSheet, Text } from 'react-native';
+import { View, StyleSheet, Text,SafeAreaView} from 'react-native';
+
 
 const AboutUs = ( ) => {
     return (
-        <View style={styles.container}>
-            <Text>We are developers~! </Text>
-        </View>
+        <SafeAreaView style ={styles.container}>
+        <Text style={styles.title}>CHANGE</Text>
+        
+            <Text>"CHANGE" app uses AI-based neural networks to transform your photos to cartoon characters.
+
+                Upload a photo from the Gallery to see the result: disney version of your photo.
+            </Text>
+        </SafeAreaView>
     )
 }
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        alignItems: 'center',
+      backgroundColor: '#fff',
+       
+    },
+    title: {
+        width: '100%',
+        height: '10%',
         justifyContent: 'center',
-    }
+        textAlign: 'center',
+        backgroundColor: 'rgb(240, 217, 215)',
+  
+        fontSize: 70,
+        color: 'black',
+        fontStyle: 'italic',
+        fontWeight: 'bold',
+      }
 })
 
 export default AboutUs;
